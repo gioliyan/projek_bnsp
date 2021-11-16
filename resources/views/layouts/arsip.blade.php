652992ca3bbad88306e5d864f69105eaa4003a17
gioliyan <gioliyan.pratama@gmail.com>
2021-11-16 18:29:55 +0700

    edit layout

--- resources/views/layouts/arsip.blade.php
+++ resources/views/layouts/arsip.blade.php
@@ -23,25 +23,25 @@
         <div class="col-md-12">
             <table class="table table-bordered table-striped">
                 <thead>
-                    <tr class=" text-center" style="background-color: rgb(66, 66, 66)">
+                    <tr class=" text-center" style="background-color: rgb(64, 64, 64)">
                         <th>Nomor Surat</th>
                         <th>Kategori</th>
                         <th>Judul</th>
                         <th>Waktu Pengerjaan</th>
-                        <th><i class="fas fa-edit"></i></th>
+                        <th>Aksi</i></th>
                     </tr>
                 </thead>
                 <tbody>
                     @foreach ($letters as $p)
-                    <tr class=" text-center" style="background-color: rgb(110, 110, 110); color">
+                    <tr class=" text-center" style="background-color: rgb(64, 64, 64); color">
                         <td class="text-light">{{ $p->number}}</td>
                         <td class="text-light">{{ $p->category->name}}</td>
                         <td class="text-light">{{ $p->title}}</td>
                         <td class="text-light">{{ $p->created_at}}</td>
                         <td>
-                            <a href="/delete/konfirmasi/{{ $p->id }}" ><span class="badge bg-danger text-l"><i class="fas fa-trash-alt"></i></span></a>
-                            <a href="/download/{{ $p->id }}"><span class="badge bg-warning text-l"><i class="fas fa-download"></i></span></a>
-                            <a href="/arsipView/{{ $p->id }}"><span class="badge bg-primary text-l"   ><i class="fas fa-eye"></i></span></a>
+                            <a href="#" ><span class="badge bg-danger text-l">Hapus</span></a>
+                            <a href="#"><span class="badge bg-warning text-l">Unduh</span></a>
+                            <a href="#"><span class="badge bg-primary text-l">Lihat>></span></a>
                         </td>
                     </tr>
                     @endforeach
